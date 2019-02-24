e62badb69ec6a5c5e3290d745e56f81fac0bc2a8
Daniel Williams <danswilliams95@gmail.com>
2019-02-24 15:08:12 -0800

    Switched to VerticalTimeline.

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/database';
 import './App.css';
+import Line from './Line/component/Line.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 global.jQuery=require('jquery');
 require('bootstrap');
@@ -11,11 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <div className="card card-default post-body">
-        <div className="card-body">
-              The Berelson Project
-        </div>
-   </div>
+         <Line/>
       </div>
     );
   }
