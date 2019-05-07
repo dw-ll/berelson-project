1d210b7359b42750669c5aea97e5049b39c238c1
Daniel Williams <danswilliams95@gmail.com>
2019-05-07 12:09:24 -0700

    fixed Router render bug where multiple children would render along with chosen route.

--- timeline/src/Line/component/Line.jsx
+++ timeline/src/Line/component/Line.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link,  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
+import { Switch } from "react-router";
 import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, NavLink, Navbar, NavbarBrand, NavItem, Nav, NavDropdown } from 'reactstrap';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
@@ -7,6 +8,7 @@ import Pre from './pre';
 import Post from './post';
 import WWII from './ww2';
 import About from './about';
+import Abram from '../../gallery/component/people/abram'
 import SvgIcon from "@material-ui/core/SvgIcon";
 import Home from '@material-ui/icons/Home.js';
 import Lodz from '../../Media/Pre-WWII/Lodz.Theatre426.jpeg'
@@ -39,98 +41,113 @@ class Line extends Component{
 render(){
     return (
       <Router>
-        <Route
-          path="/pre"
-          exact
-          render={() => {
-            return <Pre />;
-          }}
-        />
-        <Route
-          path="/ww2"
-          exact
-          render={() => {
-            return <WWII />;
-          }}
-        />
+        <Switch>
+          <Route
+            path="/pre"
+            exact
+            render={() => {
+              return <Pre />;
+            }}
+          />
+          <Route
+            path="/ww2"
+            exact
+            render={() => {
+              return <WWII />;
+            }}
+          />
 
-        <Route
-          path="/post"
-          exact
-          render={() => {
-            return <Post />;
-          }}
-        />
-        <Route
-          path="/about"
-          exact
-          render={() => {
-            return <About />;
-          }}
-        />
-        <div>
-          <VerticalTimeline>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              iconStyle={{ background: "rgb(40,49,72)", color: "#fff" }}
-              //icon={<Print/>}
-            >
-              <img src={Lodz} height="250" width="250" />
-              <h3 className="vertical-timeline-element-title">
-                Pre World War II
-              </h3>
-              <h4
-                className="vertical-timeline-element-subtitle"
-                onClick={this.renderRedirect}
-              />
-              <div>
-                <Link to="/pre">Learn More</Link>
-              </div>
-              <p />
-            </VerticalTimelineElement>
+          <Route
+            path="/post"
+            exact
+            render={() => {
+              return <Post />;
+            }}
+          />
+          <Route
+            path="/about"
+            exact
+            render={() => {
+              return <About />;
+            }}
+          />
+          <Route
+            path="/abram"
+            exact
+            render={() => {
+              return <Abram />;
+            }}
+          />
+          <div>
+            <VerticalTimeline>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{ background: "rgb(40,49,72)", color: "#fff" }}
+                //icon={<Print/>}
+              >
+                <img src={Lodz} height="250" width="250" />
+                <h3 className="vertical-timeline-element-title">
+                  Pre World War II
+                </h3>
+                <h4
+                  className="vertical-timeline-element-subtitle"
+                  onClick={this.renderRedirect}
+                />
+                <div>
+                  <Link to="/pre">Learn More</Link>
+                </div>
+                <p />
+              </VerticalTimelineElement>
 
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              iconStyle={{ background: "rgb(145,53,53)", color: "#fff" }}
-            >
-              <img src={HappyBoys} height="250" width="350" />
-              <h3 className="vertical-timeline-element-title">
-                World War II
-              </h3>
-              <div>
-                <Link to="/ww2">Learn More</Link>
-              </div>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{ background: "rgb(145,53,53)", color: "#fff" }}
+              >
+                <img src={HappyBoys} height="250" width="350" />
+                <h3 className="vertical-timeline-element-title">
+                  World War II
+                </h3>
+                <div>
+                  <Link to="/ww2">Learn More</Link>
+                </div>
 
-              <p />
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              iconStyle={{ background: "rgb(187,187,187)", color: "#fff" }}
-            >
-              <img src={Pola} height="250" width="350" />
-              <h3 className="vertical-timeline-element-title">
-                Post World War II
-              </h3>
-              <h4
-                className="vertical-timeline-element-subtitle"
-                onClick={this.renderRedirect}
-              />
-              <div>
-                <Link to="/post">Learn More</Link>
-              </div>
+                <p />
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{
+                  background: "rgb(187,187,187)",
+                  color: "#fff"
+                }}
+              >
+                <img src={Pola} height="250" width="350" />
+                <h3 className="vertical-timeline-element-title">
+                  Post World War II
+                </h3>
+                <h4
+                  className="vertical-timeline-element-subtitle"
+                  onClick={this.renderRedirect}
+                />
+                <div>
+                  <Link to="/post">Learn More</Link>
+                </div>
 
-              <p />
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              iconStyle={{ background: "rgb(233,238,201)", color: "#fff" }}
-            >
-              <img src={Sevek} height="250" width="350" />
-              <h3 className="vertical-timeline-element-title">Present</h3>
-              <p />
-            </VerticalTimelineElement>
-          </VerticalTimeline>
-        </div>
+                <p />
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{
+                  background: "rgb(233,238,201)",
+                  color: "#fff"
+                }}
+              >
+                <img src={Sevek} height="250" width="350" />
+                <h3 className="vertical-timeline-element-title">Present</h3>
+                <p />
+              </VerticalTimelineElement>
+            </VerticalTimeline>
+          </div>
+        </Switch>
       </Router>
     );
         
--- timeline/src/Line/component/pre.jsx
+++ timeline/src/Line/component/pre.jsx
@@ -1,102 +1,94 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, NavLink } from 'reactstrap';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardGroup,
+  Col,
+  Container,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Row,
+  NavLink
+} from "reactstrap";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import Abram from "../../gallery/component/people/abram.jsx";
+import "react-vertical-timeline-component/style.min.css";
 import Abram26 from "../../Media/Pre-WWII/Abram.Baigelman026.jpeg";
 import Lodz from "../../Media/Pre-WWII/Lodz.Theatre426.jpeg";
 //import {Print} from 'material-ui-icons/AccessAlarm';
 
-
-class Line extends Component{
-render(){
+class Line extends Component {
+  render() {
     return (
-         <Router>
+       <Router>
+        
         <Route
-          path="/pre"
+          path="/abram"
           exact
           render={() => {
-            return <Pre />;
+            return <Abram />;
           }}
         />
-        <Route
-          path="/ww2"
-          exact
-          render={() => {
-            return <WWII />;
-          }}
-        />
-
-        <Route
-          path="/post"
-          exact
-          render={() => {
-            return <Post />;
-          }}
-        />
-    <div>
-           
-       <div className="card card-default post-body">
-            <div className="card-body main-nav">
-                
-                Images and information from before World War II.
-                
-            </div>
+      <div>
+        <div className="card card-default post-body">
+          <div className="card-body main-nav">
+            Images and information from before World War II.
+          </div>
         </div>
         <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="Abram Baigelman was lorem ipsum."
-                    iconStyle={{ background: 'rgb(40,49,72)', color: '#fff' }}
-                    //icon={<Print/>}
-                        
-                >
-                    <img src ={Abram26} height="300" width="350"/>
-                     <h3 className="vertical-timeline-element-title">Abram Baigelman</h3>
-                    <h4 className="vertical-timeline-element-subtitle"></h4>
-                    <p>
-                    
-                    </p>
-                </VerticalTimelineElement>
-                
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    iconStyle={{ background: 'rgb(145,53,53)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">World War II</h3>
-                  
-                    <p>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Abram Baigelman was lorem ipsum."
+            iconStyle={{ background: "rgb(40,49,72)", color: "#fff" }}
+            //icon={<Print/>}
+          >
+            
+            <img src={Abram26} alt="" height="100%"  />
+            <h3 className="vertical-timeline-element-title">Abram Baigelman</h3>
+            <h4 className="vertical-timeline-element-subtitle" />
+            <div>
+              <Link to="/abram">View</Link> 
+            </div>
+            <p />
+          </VerticalTimelineElement>
 
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="1960-1970"
-                    iconStyle={{ background: 'rgb(187,187,187)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Post World War II</h3>
-                    
-                    <p>
-                
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    iconStyle={{ background: 'rgb(233,238,201)', color: '#fff' }}
-                    
-                >
-                    <h3 className="vertical-timeline-element-title">Present</h3>
-                    <p>
-                    </p>
-                </VerticalTimelineElement>
-                
-</VerticalTimeline>
-    </div>
-    </Router>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            iconStyle={{ background: "rgb(145,53,53)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">World War II</h3>
+
+            <p />
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="1960-1970"
+            iconStyle={{ background: "rgb(187,187,187)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Post World War II
+            </h3>
+
+            <p />
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            iconStyle={{ background: "rgb(233,238,201)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">Present</h3>
+            <p />
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+      </Router>
     );
-        
-}
+  }
 }
 export default Line;
