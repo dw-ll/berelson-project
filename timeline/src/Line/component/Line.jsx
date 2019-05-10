47c3ce786963a109aa272f70327b83956eeed205
Daniel Williams <danswilliams95@gmail.com>
2019-05-10 00:23:48 -0700

    implementing cards.

--- timeline/src/Line/component/Line.jsx
+++ timeline/src/Line/component/Line.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
 import { Switch } from "react-router";
-import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, NavLink, Navbar, NavbarBrand, NavItem, Nav, NavDropdown } from 'reactstrap';
+import {  CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, NavLink, Navbar, NavbarBrand, NavItem, Nav, NavDropdown } from 'reactstrap';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import Present from "./present";
@@ -9,6 +9,14 @@ import Pre from './pre';
 import Post from './post';
 import WWII from './ww2';
 import About from './about';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+
+import Typography from "@material-ui/core/Typography";
 
 import SvgIcon from "@material-ui/core/SvgIcon";
 import Home from '@material-ui/icons/Home.js';
@@ -17,23 +25,38 @@ import HappyBoys from '../../Media/WWII/HappyBoys4282.jpeg'
 import Pola from '../../Media/Post-WWII 2/pola178.jpeg'
 import Sevek from '../../Media/Modern/Sevek261.jpeg'
 //import {Print} from 'material-ui-icons/AccessAlarm';
+ const styles = {
+   card: {
+     maxWidth: 345
+   },
+   media: {
+     height: 250
+   }
+ };
 
-class Line extends Component{
-    state = {
-        redirect: false
-
+class Line extends Component {
+  state = {
+    redirect: false
+  };
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+  renderRedirect = () => {
+    if (this.state.redirect) {
     }
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
-    }
-    renderRedirect = () => {
-        if (this.state.redirect) {
+  };
   
-        }
-    }
-render(){
+  constuctor() {
+    this.routeChange = this.routeChange.bind(this);
+  }
+  routeChange() {
+    let path = `newPath`;
+    this.props.history.push(path);
+  }
+
+  render() {
     return (
       <Router>
         <Switch>
@@ -74,25 +97,48 @@ render(){
             }}
           />
 
-          <div>
+          <div className="back">
             <VerticalTimeline>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
                 iconStyle={{ background: "rgb(40,49,72)", color: "#000" }}
+                paddingTop="0em"
+                
                 //icon={<Print/>}
               >
-                <img src={Lodz} height="250" width="250" />
-                <h3 className="vertical-timeline-element-title">
-                  Pre World War II
-                </h3>
-                <h4
-                  className="vertical-timeline-element-subtitle"
-                  onClick={this.renderRedirect}
-                />
-                <div>
-                  <Link to="/pre">Learn More</Link>
+              <div>
+                <Card className="card">
+                  <CardActionArea>
+                    <CardMedia
+                    style = {{ height: 0, paddingTop: '50%'}}
+                    image= {require ("../../Media/Pre-WWII/Lodz.Theatre426.jpeg")}
+                  
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Pre-World War II
+                      </Typography>
+                      <Typography component="p">
+                        Photos and information spanning the time before
+                        World War II.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      color="primary"
+                      size="small"
+                      color="primary"
+                      component={Link}
+                      to="/pre"
+                    >
+                      Learn More
+                    </Button>
+                  </CardActions>
+                </Card>
                 </div>
-                <p />
+
               </VerticalTimelineElement>
 
               <VerticalTimelineElement
@@ -149,7 +195,6 @@ render(){
         </Switch>
       </Router>
     );
-        
-}
+  }
 }
 export default Line;
