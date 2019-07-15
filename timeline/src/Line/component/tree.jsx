30853a3fead19c17dbcc3806b4451d39537e6337
Daniel Williams <danswilliams95@gmail.com>
2019-07-15 16:28:55 -0700

    Working on family tree, fix scaling.

--- timeline/src/Line/component/tree.jsx
+++ timeline/src/Line/component/tree.jsx
@@ -8,40 +8,167 @@ import WWII from "./ww2";
 
 const myTreeData = [
   {
-    name: 'Top Level',
+    name: "Koppel Multzman",
     attributes: {
-      keyA: 'val A',
-      keyB: 'val B',
-      keyC: 'val C',
+      keyA: "Koppel Multzman",
+      keyB: "Chana"
     },
     children: [
       {
-        name: 'Level 2: A',
+        name: "Level 2: A",
         attributes: {
-          keyA: 'val A',
-          keyB: 'val B',
-          keyC: 'val C',
-        },
+          keyA: "Pinchas",
+          keyB: "Freide",
+          keyC: "Pessel",
+          keyD: "Rosa"
+        }
       },
       {
-        name: 'Level 2: B',
-      },
-    ],
-  },
+        name: "Level 2: B"
+      }
+    ]
+  }
 ];
- 
-class MyComponent extends Component {
+
+class FamilyTree extends Component {
   render() {
     return (
-  
+      <div class="Treewrapper">
+          <div class ="treecase">
+        <div class="tree">
+          <ul>
+            <li>
+              <a href="#">Koppel</a>
+              <ul>
+                <li>
+                  <a href="#">Pinchas</a>
+                  <ul>
+                    <li>
+                      <a href="#">Katz</a>
+                      <ul>
+                        <li>
+                          <a href="#">Abram</a>
+                        </li>
+                        <li>
+                          <a href="#">? (Brother)</a>
+                          <ul>
+                            <li>
+                              <a href="#">Michael</a>
+                              <ul>
+                                <li>
+                                  <a href="#">Married?</a>
+                                  <a href="#">Putty</a>
+                                </li>
+                              </ul>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Freide</a>
+                </li>
+                <li>
+                  <a href="#">Pessel</a>
+                </li>
+                <li>
+                  <a href="#">Rosa</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Chana</a>
+              <ul>
+                <li>
+                  <a href="#">Simon (Married Riva)</a>
+                  <ul>
+                    <li>
+                      <a href="#">David (Married Chana Federman)</a>
+                      <ul>
+                        <li>
+                          <a href="#">Dinchaz B. Lodz</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#">Chaya</a>
+                      <ul>
+                        <li>
+                          <a href="#">Liebel</a>
+                        </li>
+                        <li>
+                          <a href="#">Anja</a>
+                        </li>
+                        <li>
+                          <a href="#">Shlameck</a>
+                        </li>
+                        <li>
+                          <a href="#">Rose</a>
+                        </li>
+                        <li>
+                          <a href="#">Lola</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#">Henry (Married Greta Glazer)</a>
+                      <ul>
+                        <li>
+                          <a href="#">Liebel</a>
+                        </li>
+                        <li>
+                          <a href="#">Anja</a>
+                        </li>
+                        <li>
+                          <a href="#">Shlameck</a>
+                        </li>
+                        <li>
+                          <a href="#">Rose</a>
+                        </li>
+                        <li>
+                          <a href="#">Lola</a>
+                        </li>
+                      </ul>
+                    </li>
 
-        
-      <div id="treeWrapper" style={{width: '50em', height: '20em'}}>
- 
-        <Tree data={myTreeData} />
- 
+                    <li>
+                      <a href="#">Shlomo B. Lodz</a>
+                    </li>
+                    <li>
+                      <a href="#">Eita</a>
+                    </li>
+                    <li>
+                      <a href="#">Rosa</a>
+                    </li>
+                    <li>
+                      <a href="#">Avram</a>
+                    </li>
+                    <li>
+                      <a href="#">Chaina</a>
+                    </li>
+                    <li>
+                      <a href="#">Chamon?</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Leizer (Married Ida)</a>
+                </li>
+                <li>
+                  <a href="#">Maika (Married Rochverg [Frank])</a>
+                </li>
+                <li>
+                  <a href="#">Yessel (Married Paige Laufer)</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        </div>
       </div>
     );
   }
 }
-export default MyComponent;
+export default FamilyTree;
