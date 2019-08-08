6a70d908d8aa996690e510d23df302d3a7951da9
Daniel Williams <danswilliams95@gmail.com>
2019-08-08 15:39:44 -0700

    WW2 profiles restyled.

--- timeline/src/gallery/component/people/ww2_happy.jsx
+++ timeline/src/gallery/component/people/ww2_happy.jsx
@@ -17,19 +17,26 @@ const Slideshow = () => {
             return <WWII />;
           }}
         />
-        <div>
-          <img className="single_photo" src={slideImages} alt="" />
-          <h6>
-            Happy Boys was lorem ipsumdolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
-          </h6>
-          <Link to="/ww2">Go back to World War II</Link>
+        <div class="container">
+          <div class="row">
+            <div class="col-6">
+              <h3 className="profile_title">Happy Boys</h3>
+              <h6>B. 1900 D. 1950</h6>
+              <h6 className="profile_bio">
+                Happy Boys was lorem ipsumdolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </h6>
+            </div>
+            <div class="col-6">
+              <img className="single_photo" src={slideImages} alt="" />
+            </div>
+          </div>
         </div>
       </Switch>
     </Router>
