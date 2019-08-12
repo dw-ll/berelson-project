5024f2c741d514f6f0b6bd84549a648cf30e9120
Daniel Williams <danswilliams95@gmail.com>
2019-08-12 10:44:18 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/berelson-project/precache-manifest.ce6d44518e54b2926e3591f02a769351.js"
+  "/berelson-project/precache-manifest.d902aea229d8e77b4a7a9a4db858839d.js"
 );
 
 workbox.clientsClaim();
