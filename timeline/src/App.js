087c87d556bbfd5ad53171e5006f23e39c168dc9
Daniel Williams <danswilliams95@gmail.com>
2019-10-18 15:53:24 -0700

    Added music player.

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -11,6 +11,8 @@ import { NavLink, Navbar, NavbarBrand, NavItem, Nav } from "reactstrap";
 import _ from "lodash";
 import { Search } from "semantic-ui-react";
 import SearchBar from "react-search-bar-semantic-ui";
+import MusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Pre from "./Line/component/pre.jsx";
@@ -61,7 +63,168 @@ const searchData = [
     image: require("../src/Media/Modern/Riva346.jpeg")
   }
 ];
+const options = {
+  //audio lists model
+  audioLists: [
+    {
+      name: "Despacito",
+      singer: "Luis Fonsi",
+      cover:
+        "http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg",
+      musicSrc: () => {
+        return Promise.resolve(
+          "http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3"
+        );
+      }
+    },
+    {
+      name: "Bedtime Stories",
+      singer: "Jay Chou",
+      cover:
+        "http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg",
+      musicSrc:
+        "http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3"
+    }
+  ],
 
+  //default play index of the audio player  [type `number` default `0`]
+  defaultPlayIndex: 0,
+
+  //if you want dynamic change current play audio you can change it [type `number` default `0`]
+  // playIndex: 0,
+
+  //color of the music player theme    [ type `string: 'light' or 'dark'  ` default 'dark' ]
+  theme: "light",
+
+  // Specifies movement boundaries. Accepted values:
+  // - `parent` restricts movement within the node's offsetParent
+  //    (nearest node with position relative or absolute), or
+  // - a selector, restricts movement within the targeted node
+  // - An object with `left, top, right, and bottom` properties.
+  //   These indicate how far in each direction the draggable
+  //   can be moved.
+  bounds: "body",
+
+  //Whether to load audio immediately after the page loads.  [type `Boolean | String`, default `false`]
+  //"auto|metadata|none" "true| false"
+  preload: false,
+
+  //Whether the player's background displays frosted glass effect  [type `Boolean`, default `false`]
+  glassBg: true,
+
+  //The next time you access the player, do you keep the last state  [type `Boolean` default `false`]
+  remember: false,
+
+  //The Audio Can be deleted  [type `Boolean`, default `true`]
+  remove: true,
+
+  //audio controller initial position    [ type `Object` default '{top:0,left:0}' ]
+  defaultPosition: {
+    top: 700,
+    left: 55
+  },
+
+  // play mode text config of the audio player
+  playModeText: {
+    order: "In Order",
+    orderLoop: "Loop",
+    singleLoop: "Loop Once",
+    shufflePlay: "Shuffle"
+  },
+
+  //audio controller open text  [ type `String | ReactNode` default 'open']
+  openText: "Open",
+
+  //audio controller close text  [ type `String | ReactNode` default 'close']
+  closeText: "Close",
+
+  // audio list panel show text of the playlist has no songs [ type `String` | ReactNode  default 'no music']
+  notContentText: "List Empty",
+
+  panelTitle: "Music",
+
+  defaultPlayMode: "order",
+
+  //audio mode        mini | full          [type `String`  default `mini`]
+  mode: "full",
+
+  /**
+   * [ type `Boolean` default 'false' ]
+   * The default audioPlay handle function will be played again after each pause, If you only want to trigger it once, you can set 'true'
+   */
+  once: true,
+
+  //Whether the audio is played after loading is completed. [type `Boolean` default 'true']
+  autoPlay: false,
+
+  //Whether you can switch between two modes, full => mini  or mini => full   [type 'Boolean' default 'true']
+  toggleMode: true,
+
+  //audio cover is show of the "mini" mode [type `Boolean` default 'true']
+  showMiniModeCover: false,
+
+  //audio playing progress is show of the "mini"  mode
+  showMiniProcessBar: false,
+
+  //audio controller is can be drag of the "mini" mode     [type `Boolean` default `true`]
+  drag: true,
+
+  //drag the audio progress bar [type `Boolean` default `true`]
+  seeked: true,
+
+  //Displays the audio load progress bar.  [type `Boolean` default `true`]
+  showProgressLoadBar: true,
+
+  //play button display of the audio player panel   [type `Boolean` default `true`]
+  showPlay: true,
+
+  //reload button display of the audio player panel   [type `Boolean` default `true`]
+  showReload: true,
+
+  //download button display of the audio player panel   [type `Boolean` default `true`]
+  showDownload: false,
+
+  //loop button display of the audio player panel   [type `Boolean` default `true`]
+  showPlayMode: true,
+
+  //theme toggle switch  display of the audio player panel   [type `Boolean` default `true`]
+  showThemeSwitch: false,
+
+  //lyric display of the audio player panel   [type `Boolean` default `false`]
+  showLyric: false,
+
+  //Extensible custom content       [type 'Array' default '[]' ]
+  extendsContent: [],
+
+  //default volume of the audio player [type `Number` default `100` range `0-100`]
+  defaultVolume: 100,
+
+  //playModeText show time [type `Number(ms)` default `700`]
+  playModeShowTime: 600,
+
+  //Whether to try playing the next audio when the current audio playback fails [type `Boolean` default `true`]
+  loadAudioErrorPlayNext: true,
+
+  onAudioListsChange(currentPlayId, audioLists, audioInfo) {
+    console.log("[currentPlayId] audio lists change:", currentPlayId);
+    console.log("[audioLists] audio lists change:", audioLists);
+    console.log("[audioInfo] audio lists change:", audioInfo);
+  },
+
+  onAudioPlayTrackChange(currentPlayId, audioLists, audioInfo) {
+    console.log(
+      "audio play track change:",
+      currentPlayId,
+      audioLists,
+      audioInfo
+    );
+  },
+
+  // custom music player root node
+  getContainer() {
+    return document.body;
+  }
+};
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [done, setDone] = useState(false);
@@ -71,6 +234,7 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(fetchInitMode());
   const [open, setOpen] = useState(false);
   const [person, setPerson] = useState("");
+  const [params, setParams] = useState(options);
 
   const handleClose = () => {
     setOpen(false);
@@ -439,6 +603,9 @@ const App = () => {
               )}
             />
             <div>
+              <div>
+                <MusicPlayer className="music-player" {...params} />
+              </div>
               <div>
                 <ScrollButton
                   className="scroll-up-button"
