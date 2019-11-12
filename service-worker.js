692053136e00bee5de158b1cc9f6b8ab34ed9ad7
Daniel Williams <danswilliams95@gmail.com>
2019-11-12 11:05:48 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3161b05f2d99ca19d38bd02fc8caa956.js"
+  "/precache-manifest.34c24e2c69eb3a8f946cbac079850753.js"
 );
 
 workbox.clientsClaim();
