edef2ca2770e755bfcc4853fb89cb697e20ae8a1
Daniel Williams <danswilliams95@gmail.com>
2019-11-13 13:43:48 -0800

    Edited spanned formatting. Switched to static landing page greeting.

--- timeline/src/Line/component/span.jsx
+++ timeline/src/Line/component/span.jsx
@@ -9,6 +9,11 @@ import {
   MDBCarouselItem,
   MDBView
 } from "mdbreact";
+import Greeting from "react-lazy-hero";
+import Lodz from "../../Media/lodz.jpg";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import FoniaGreet from "../../Media/Post-WWII 2/Fonia213.jpeg";
 import CarolGreet from "../../Media/Modern/Carol343.jpeg";
 import EdGreet from "../../Media/Post-WWII 2/Ed.Silver091.jpeg";
@@ -19,124 +24,43 @@ export default class Span extends Component {
     return (
       <div>
         <div className="greet">
-          <MDBCarousel
-            activeItem={1}
-            length={4}
-            interval={2500}
-            showControls={false}
-            showIndicators={false}
-            className="carousel z-depth-1"
-            style={{ height: "750px", textAlign: "center" }}
+          <Greeting
+            minHeight="100vh"
+            opacity="0"
+            isCentered={true}
+            imageSrc={Lodz}
+            className="greeting"
           >
-            <MDBCarouselInner>
-              <MDBCarouselItem itemId="1">
-                <MDBView className="img-slider">
-                  <img
-                    className="d-block w-100"
-                    src={FoniaGreet}
-                    alt="First slide"
-                  />
-                </MDBView>
-
-                <MDBCarouselCaption>
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-sm-6"></div>
-                      <div class="col-sm-6"></div>
-                    </div>
-                  </div>
-                  <div
-                    className="scroll-down"
-                    onClick={() => {
-                      this.scrollDiv.current.scrollIntoView({
-                        behavior: "smooth"
-                      });
-                    }}
-                  ></div>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="2">
-                <MDBView className="img-slider">
-                  <img
-                    className="d-block w-100"
-                    src={Fonia2Greet}
-                    alt="First slide"
-                  />
-                </MDBView>
-
-                <MDBCarouselCaption>
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-sm-6"></div>
-                      <div class="col-sm-6"></div>
-                    </div>
-                  </div>
-                  <div
-                    className="scroll-down"
-                    onClick={() => {
-                      this.scrollDiv.current.scrollIntoView({
-                        behavior: "smooth"
-                      });
-                    }}
-                  ></div>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="3">
-                <MDBView>
-                  <img
-                    className="d-block w-100"
-                    src={CarolGreet}
-                    alt="Second slide"
-                  />
-                </MDBView>
-                <MDBCarouselCaption>
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-sm-6"></div>
-                      <div class="col-sm-6"></div>
-                    </div>
-                  </div>
-                  <div
-                    className="scroll-down"
-                    onClick={() => {
-                      this.scrollDiv.current.scrollIntoView({
-                        behavior: "smooth"
-                      });
-                    }}
-                  ></div>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-              <MDBCarouselItem itemId="4">
-                <MDBView>
-                  <img
-                    className="d-block w-100"
-                    src={EdGreet}
-                    alt="Third slide"
-                  />
-                </MDBView>
-                <MDBCarouselCaption>
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-sm-6"></div>
-                      <div class="col-sm-6"></div>
-                    </div>
-                  </div>
-                  <div
-                    className="scroll-down"
-                    onClick={() => {
-                      this.scrollDiv.current.scrollIntoView({
-                        behavior: "smooth"
-                      });
-                    }}
-                  ></div>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-            </MDBCarouselInner>
-          </MDBCarousel>
+            <div className="greeting-greet">
+              <h3
+                className="greeting-text"
+                style={{
+                  color: "black",
+                  fontFamily: "Times",
+                  marginTop: "10px"
+                }}
+              >
+                The Berelson Project
+              </h3>
+              <div className="greeting-button">
+                <Grid item>
+                  <ButtonGroup aria-label="Outlined primary button group">
+                    <Button
+                      component={Link}
+                      to="/archive"
+                      className="greeting-button-text"
+                    >
+                      Visit Archive
+                    </Button>
+                  </ButtonGroup>
+                </Grid>
+              </div>
+            </div>
+          </Greeting>
         </div>
         <ul className="eras">
           <li className="era-span">
-            <a className="era-image">&nbsp;</a>
+            <a className="era-image1">&nbsp;</a>
             <div className="l-container era-text-container">
               <div className="page-section__row era-text-inner-container">
                 <div className="page-section__title-container era-text-break"></div>
@@ -161,7 +85,84 @@ export default class Span extends Component {
               </div>
             </div>
           </li>
-          <li className="era-span"></li>
+          <li className="era-span">
+            <a className="era-image2">&nbsp;</a>
+            <div className="l-container era-text-container">
+              <div className="page-section__row era-text-inner-container">
+                <div className="page-section__title-container era-text-break"></div>
+                <div className="page-section__content era-text">
+                  <h3 className="era-text-link">World War II</h3>
+                  <p className="era-date">
+                    <span>1900-1950</span>
+                  </p>
+                  <div className="rich-text-desc">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="era-span">
+            <a className="era-image3">&nbsp;</a>
+            <div className="l-container era-text-container">
+              <div className="page-section__row era-text-inner-container">
+                <div className="page-section__title-container era-text-break"></div>
+                <div className="page-section__content era-text">
+                  <h3 className="era-text-link">After the War</h3>
+                  <p className="era-date">
+                    <span>1900-1950</span>
+                  </p>
+                  <div className="rich-text-desc">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="era-span">
+            <a className="era-image4">&nbsp;</a>
+            <div className="l-container era-text-container">
+              <div className="page-section__row era-text-inner-container">
+                <div className="page-section__title-container era-text-break"></div>
+                <div className="page-section__content era-text">
+                  <h3 className="era-text-link">Present</h3>
+                  <p className="era-date">
+                    <span>1900-1950</span>
+                  </p>
+                  <div className="rich-text-desc">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     );
