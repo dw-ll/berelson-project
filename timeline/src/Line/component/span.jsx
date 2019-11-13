571848b8feb0e9fe2e4379bd339641564bb05490
Daniel Williams <danswilliams95@gmail.com>
2019-11-13 13:04:21 -0800

    Working spanned layout.

--- timeline/src/Line/component/span.jsx
+++ timeline/src/Line/component/span.jsx
@@ -134,15 +134,31 @@ export default class Span extends Component {
             </MDBCarouselInner>
           </MDBCarousel>
         </div>
-        <div className="eras-title">Berelson Lineage in Eras</div>
         <ul className="eras">
           <li className="era-span">
             <a className="era-image">&nbsp;</a>
-            <div className="era-text-container">
-              <div className="era-text-break"></div>
-              <div className="era-text">
-                <h3 className="era-text-link">Before the War</h3>
+            <div className="l-container era-text-container">
+              <div className="page-section__row era-text-inner-container">
+                <div className="page-section__title-container era-text-break"></div>
+                <div className="page-section__content era-text">
+                  <h3 className="era-text-link">Before the War</h3>
+                  <p className="era-date">
+                    <span>1900-1950</span>
+                  </p>
+                  <div className="rich-text-desc">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </div>
                 </div>
+              </div>
             </div>
           </li>
           <li className="era-span"></li>
