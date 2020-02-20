ae9ccd819fbd3de22f1d9672717a11f045b4cf81
Daniel Williams <danswilliams95@gmail.com>
2020-02-19 17:03:09 -0800

    Reformatted landing page profiles.

--- src/component/landing_span.jsx
+++ src/component/landing_span.jsx
@@ -1,6 +1,6 @@
 import React, { Component, createRef } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Card } from "semantic-ui-react";
+import { Button, Card } from "react-bootstrap";
 import { Switch } from "react-router";
 import Greeting from "react-lazy-hero";
 import Lodz from "../Media/lodz.jpg";
@@ -8,11 +8,11 @@ import Pre from "./pre.jsx";
 import WW2 from "./ww2.jsx";
 import Post from "./post.jsx";
 import Modern from "./present.jsx";
-import DavidProfile from "../Media/Pre-WWII/David.Baigelman016.jpeg";
+import DavidProfile from "../Media/Pre-WWII/DavidProfile.jpeg";
 import DavidRouter from "../profiles/component/people/pre_david.jsx";
-import HenryProfile from "../Media/Post-WWII 2/Henry.Baigelman119.jpeg";
+import HenryProfile from "../Media/Post-WWII 2/Henry.Baigelman119 2.jpeg";
 import HenryRouter from "../profiles/component/people/pre_henry.jsx";
-import RivaProfile from "../Media/Modern/Riva346.jpeg";
+import RivaProfile from "../SearchMedia/Riva/pola193.jpeg";
 import RivaRouter from "../profiles/component/people/present_riva.jsx";
 const routes = [
   {
@@ -73,6 +73,7 @@ export default class Span extends Component {
               <Greeting
                 opacity="0"
                 isCentered={true}
+                parallaxOffset={200}
                 imageSrc={Lodz}
                 className="greeting"
               >
@@ -81,11 +82,14 @@ export default class Span extends Component {
                     className="greeting-text"
                     style={{
                       color: "black",
-                      fontFamily: "Times",
-                      marginTop: "10px"
+                      fontFamily: "Cambria",
+                      marginTop: "7px",
+                      marginLeft: "2px",
+                      letterSpacing: "2.5px",
+                      fontVariant: "small-caps"
                     }}
                   >
-                    Vessel Archives
+                    vessel archives
                   </h3>
 
                   <div className="greeting-button"></div>
@@ -100,142 +104,57 @@ export default class Span extends Component {
                 </div>
               </Greeting>
             </div>
-            <ul className="eras" ref={this.scrollDiv}>
-              <h4 style={{ textAlign: "center" }} className="intro-text">
-                meet the family
-              </h4>
-              <li className="era-span2 left">
-                <div class="landing_container left">
-                  <div class="ui cards">
-                    <Card
-                      fluid
-                      color="transparent"
-                      header="Option 1"
-                      className="profile-card"
-                    >
-                      <div class="content">
-                        <div class="row">
-                          <div class="col-6">
-                            <Link to="/pre/david/">
-                              <img
-                                className="single-photo-landing"
-                                src={DavidProfile}
-                                alt=""
-                              />
-                            </Link>
-                          </div>
-                          <div class="col-6 landing-profile-text">
-                            <h3 className="profile_title">David Baigelman</h3>
-                            <h6>B. 1900 D. 1950</h6>
-                            <h5 className="profile_bio_profile">
-                              David Baigelman was an extremely versatile person:
-                              a violinist and violist, a conductor, composer and
-                              songwriter. He conducted the Łódź Symphony
-                              Orchestra and the orchestra of the Jewish Icchok
-                              Zandberg Theatre, he composed music for many
-                              theatres all over Poland and conducted their
-                              orchestras, he recorded albums for Syrena-Electro,
-                              but became famous as the author of the music for
-                              the performance of The Dybbuk by S. Anski, which
-                              became an international theatre hit. During the
-                              war, he founded and conducted a 44-member symphony
-                              orchestrain the Łódź Ghetto.
-                            </h5>
-                          </div>
-                        </div>
-                      </div>
-                    </Card>
-                  </div>
+            <h4 style={{ textAlign: "center" }} className="intro-text">
+              meet the family
+            </h4>
+            <div class="container" style={{ marginTop: "35px" }}>
+              <div class="row profiles">
+                <div class="col-4">
+                  <Card>
+                    <Link to="/pre/david/">
+                      <Card.Img src={DavidProfile}></Card.Img>
+                    </Link>
+                    <Card.Body>
+                      <Card.Title>David Baigelman</Card.Title>
+                      <Card.Text>
+                        A violinist, conductor, composer and songwriter
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
                 </div>
-              </li>
-              <div className="space"></div>
-              <li className="era-span2 right">
-                <div class="landing_container right">
-                  <div class="ui cards">
-                    <Card
-                      fluid
-                      color="transparent"
-                      header="Option 1"
-                      className="profile-card"
-                    >
-                      <div class="content">
-                        <div class="row">
-                          <div class="col-6">
-                            <Link to="/pre/henry/">
-                              <img
-                                className="single-photo-landing"
-                                src={HenryProfile}
-                                alt=""
-                              />
-                            </Link>
-                          </div>
-                          <div class="col-6 landing-profile-text">
-                            <h3 className="profile_title">Henry Baigelman</h3>
-                            <h6>B. 1900 D. 1950</h6>
-                            <h5 className="profile_bio_profile">
-                              Chaim (later: Henry) Baigelman was a violinist,
-                              saxophonist and composer and the only one out of
-                              nine musical siblings to survive the war. He
-                              played professionally since the age of fifteen. He
-                              was a member of a family band, The Jolly Boys,
-                              which he revived in post-war Germany under the
-                              name The Happy Boys, playing jazz concerts for
-                              American soldiers and Jewish survivors. After the
-                              war, he emigrated to New York, where he continued
-                              his musical activities with The Happy Boys.
-                            </h5>
-                          </div>
-                        </div>
-                      </div>
-                    </Card>
-                  </div>
+                <div class="col-4" />
+                <div class="col-4">
+                  <Card>
+                    <Link to="/pre/henry/">
+                      <Card.Img src={HenryProfile}></Card.Img>
+                    </Link>
+                    <Card.Body>
+                      <Card.Title>Henry Baigelman</Card.Title>
+                      <Card.Text>
+                        A violinist, saxophonist and composer and the only one
+                        out of nine musical siblings to survive the war
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
                 </div>
-              </li>
-              <div className="space"></div>
-              <li className="era-span2 left">
-                <div class="landing_container left">
-                  <div class="ui cards">
-                    <Card
-                      fluid
-                      color="transparent"
-                      header="Option 1"
-                      className="profile-card"
-                    >
-                      <div class="content">
-                        <div class="row">
-                          <div class="col-6">
-                            <Link to="/present/riva/">
-                              <img
-                                className="single-photo-landing"
-                                src={RivaProfile}
-                                alt=""
-                              />
-                            </Link>
-                          </div>
-                          <div class="col-6 landing-profile-text">
-                            <h3 className="profile_title">Riva Berelson</h3>
-                            <h6>B. 1900 D. 1950</h6>
-                            <h5 className="profile_bio_profile">
-                              Riva was lorem ipsumdolor sit amet, consectetur
-                              adipiscing elit, sed do eiusmod tempor incididunt
-                              ut labore et dolore magna aliqua. Ut enim ad minim
-                              veniam, quis nostrud exercitation ullamco laboris
-                              nisi ut aliquip ex ea commodo consequat. Duis aute
-                              irure dolor in reprehenderit in voluptate velit
-                              esse cillum dolore eu fugiat nulla pariatur.
-                              Excepteur sint occaecat cupidatat non proident,
-                              sunt in culpa qui officia deserunt mollit anim id
-                              est laborum.
-                            </h5>
-                          </div>
-                        </div>
-                      </div>
-                    </Card>
-                  </div>
-                </div>
-              </li>
-              <div className="space"></div>
-            </ul>
+              </div>
+            </div>
+            <div class="row profiles">
+              <div class="col-4" />
+              <div class="col-4">
+                {" "}
+                <Card>
+                  <Link to="/present/riva">
+                    <Card.Img src={RivaProfile}></Card.Img>
+                  </Link>
+                  <Card.Body>
+                    <Card.Title>Riva Baigelman</Card.Title>
+                    <Card.Text>More info about Riva.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div class="col-4" />>
+            </div>
           </div>
         </Switch>
       </Router>
